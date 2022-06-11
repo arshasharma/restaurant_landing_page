046141617d40820579138af52327681b9c6e8b52
Arsha Sharma <52933661+arshasharma@users.noreply.github.com>
2022-06-11 23:15:29 +0530

    Add files via upload

---
+++ omni food/resources/script.js
@@ -0,0 +1,16 @@
+$('.js--section-feature').waypoint(function(direction){
+    if (direction =="down") {
+        $('nav').addClass('sticky');  
+    } else {
+        $('nav').removeClass('sticky');
+    } 
+},{
+    offset:'60px;'
+});
+/**scroll on buttons */
+$('.js--scroll-to-plans') .click(function() {
+    $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+});
+
+
+
